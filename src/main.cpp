00bb2c57b80c0e73a0ba5ae2d27f3605ee0e22fd
quartz55 <john.razor97@gmail.com>
2015-05-31 02:38:14 +0100

    Added help screen 'legrep --help'

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,19 @@
 
 using namespace std;
 
+void printHelp(char *argv[]);
+
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " [OPTION]... PATTERN [FILE]\n";
+        std::cout << "Usage: " << argv[0] << " [OPTION]... PATTERN [FILE]...\n";
+        std::cout << "Try '" << argv[0] << " --help' for more information.\n";
+        return 1;
+    }
+
+    // Show help screen
+    if (string(argv[1]) == "--help") {
+        printHelp(argv);
         return 1;
     }
 
@@ -23,4 +32,31 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+void printHelp(char *argv[])
+{
+    std::cout << "Usage: " << argv[0] << " [OPTION]... PATTERN [FILE]...\n";
+    std::cout << "Search for PATTERN in each FILE or standard input.\n";
+    std::cout << "PATTERN is, by default, a basic regular expression (BRE).\n";
+    std::cout << "Example: " << argv[0] << " -i 'estrutura' text1.txt\n";
+
+    std::cout << "\n";
+    std::cout << "Regexp selection and interpretation:\n";
+    std::cout
+        << "  -i, --ignore-case" << "\t\tignore case distinctions" << "\n"
+        ;
+
+    std::cout << "\n";
+    std::cout << "Miscellaneous:\n";
+    std::cout
+        << "  -v, --invert-match" << "\t\tselect non-matching lines" <<  "\n"
+        ;
+
+    std::cout << "\n";
+    std::cout << "Context control:\n";
+    std::cout
+        << "  -B NUM, --before-context=NUM" << "\tprint NUM lines of leading context" << "\n"
+        << "  -A NUM, --after-context=NUM" << "\tprint NUM lines of trailing context" << "\n"
+        ;
 }
